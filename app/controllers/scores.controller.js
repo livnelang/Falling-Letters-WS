fe14a3b2cabf0bc8d2df199adc23ba1ac8b5c6bc
Livne Lang <livnelang@gmail.com>
2017-06-02 16:30:00 +0300

    errors handling and fetching function

--- app/controllers/scores.controller.js
+++ app/controllers/scores.controller.js
@@ -16,16 +16,16 @@ exports.insertRecord = function (req,res)  {
     // new record from the req.body parts
     var newScore = new Scores(req.body);
 
-    newScore.save(function (err,doc) {
-        if(err) {
-            res.json(err.errors.result.message);
+    newScore.save(function (error, doc) {
+        if(error) {
+            res.status(500).json(fetchErrors(error));
         }
         else {
             Scores.find({}).sort({'result': -1
             }).limit(4)
                 .exec(function(err, results){
                 if(err) {
-                    res.json(err.errors.result.message);
+                    res.json(err.errors);
                 }
                 else {
                     res.json(results);
@@ -33,4 +33,17 @@ exports.insertRecord = function (req,res)  {
             });
         }
     });
+
+    /**
+     * fetching the error response with the flase fields and messages
+     * @param arr - our error object from the query
+     * @returns {{}}
+     */
+    function fetchErrors(arr) {
+        var err_res = {};
+        for(var err in arr.errors) {
+            err_res[err] = arr.errors[err].message;
+        }
+        return err_res;
+    }
 };
